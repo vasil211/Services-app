61c2419006c36875bbaf6b18b82a48cc891a3ffe
vasil211 <vasil.vazonov@abv.bg>
2022-03-31 23:40:21 +0300

    31.03.2022

--- src/app/Main.java
+++ src/app/Main.java
@@ -47,64 +47,6 @@ public class Main {
         RatingsController ratingsController = new RatingsController(ratingService);
         PostsController postsController = new PostsController(categoryService, postService, userService, categoryValidator, ratingService);
         HomeController homeController = new HomeController(userService, adminController, appointmentsService, postService, postsController, appointmentController, ratingsController);
-//
-//        System.out.println("Getting all users:");
-//        userService.getAll().forEach(e -> System.out.println(e.toString()));
-//
-//
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-//        StringJoiner sj = new StringJoiner("", "Messages: ", " ");
-//
-//        var messages = messageService.messagesChat(3l, 4l);
-//        User user = new User();
-//        try {
-//            user = userService.getUserById(4l);
-//        } catch (NonexistingEntityException e) {
-//            e.printStackTrace();
-//        }
-//        for (var message : messages) {
-//            if (message.getSender() == 3l) {
-//                sj.add("\nMe: " + message.getMessage() + "\n" + message.getSent().format(formatter) + "\n");
-//            } else {
-//                sj.add("\n" + user.getFirstName() + " " + user.getLastName() + ":\n" + message.getMessage() + "\n" + "    " + message.getSent().format(formatter) + "\n");
-//            }
-//        }
-//        System.out.println("Messages between user and service provider:");
-//        System.out.println(sj);
-//        AtomicReference<Post> post = new AtomicReference<>(new Post());
-//        System.out.println("Getting all 'FINISHED' appointments:");
-//        appointmentsRepository.findAllFinished(3l).forEach(e -> {
-//            try {
-//                post.set(postService.getPostById(e.getPostId()));
-//            } catch (NonexistingEntityException ex) {
-//                ex.printStackTrace();
-//            }
-//            System.out.println(e.toString() + " for job: " + post.get().getName());
-//        });
-//        User user1 = new User();
-//        User user2 = new User();
-//        try {
-//            user1 = userService.getUserById(4l);
-//        } catch (NonexistingEntityException e) {
-//            e.printStackTrace();
-//        }
-//        try {
-//            user2 = userService.getUserById(3l);
-//        } catch (NonexistingEntityException e) {
-//            e.printStackTrace();
-//        }
-//        Post post1 = new Post();
-//        try {
-//            post1 = postService.getPostById(1l);
-//        } catch (NonexistingEntityException e) {
-//            e.printStackTrace();
-//        }
-//
-//        Rating rating = new Rating(1l, user1, user2, post1, (short) 0, "asdasdasdasdasdasdasdasdscaasdasawdadwadwadwadwadwdwadwaawdawfafwafwafwafdffdfdsdfssdfsdffsdsdfsdfsdsdggsdgsdgsdgsgsgffgfgfgfggfgffgfgfggfgfgfgfggfgfgfgfffdfdddgdgdgdasdasdasdasdasdasdasdasdasdwqdqwdwqdwqdqwdddwqdwqdqwdq  qwcewvc  crewrcwcwercwercewcewrcc  qwcrewcwcweqcewqcwewqrvtcqwrvctv  tccewcettwtcwqtecwqcqceqwtctqwcqcqwecttcqtctvvcqwecqewv   ctewcrewrcwr", LocalDateTime.now(), LocalDateTime.now());
-//        System.out.println("\n\nError creating rating when invalid rating is given:");
-//        ratingService.createRating(rating);
-
-
 
 
 
